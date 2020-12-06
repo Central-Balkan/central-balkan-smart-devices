9119e7ef89fd0a984c2b17b1540b3c5cc024d1a9
Ivo Donchev <i.donchev@hacksoft.io>
2020-12-06 11:23:28 +0200

    Add updates to app

--- app/central-balkan-uvlamp-manager/App.js
+++ app/central-balkan-uvlamp-manager/App.js
@@ -9,11 +9,18 @@ import {
   Image,
   ActivityIndicator,
 } from "react-native";
+import * as axios from 'axios';
+
+
+
+// const domain = 'http://192.168.4.1';
+const domain = 'http://localhost:8000';
+// const domain = 'http://0.0.0.0:8000';
 
 const setConfig = (work, rest) => {
   console.log(work, rest);
   console.log(JSON.stringify({ work, rest }));
-  fetch("http://192.168.4.1/config/set/", {
+  fetch(`${domain}/config/set/`, {
     method: "POST",
     mode: "no-cors",
     headers: {
@@ -38,15 +45,16 @@ async function fetchWithTimeout(resource, options) {
   });
   clearTimeout(id);
 
-  return response;
+ return response;
 }
+
 
 export default class App extends React.Component {
   state = {
-    isOnline: false,
+    isOnline: true,
     workTime: null,
     restTime: null,
-    isOn: false,
+    isOn: true,
     timeLeft: 0,
 
     isInitiallyLoading: true,
@@ -61,34 +69,57 @@ export default class App extends React.Component {
     }).start();
   };
 
-  connect = () =>
-    fetchWithTimeout("http://192.168.4.1/config/get/", {
-      method: "GET",
-      mode: "no-cors",
+  connect = () => {
+  return fetch(`${domain}/config/get/`)
+    .then((response) => response.json())
+    .then((json) => {
+        console.log('asd')
+        console.log(json)
+            this.setState({
+              isOnline: true,
+              isOn: true,
+              workTime: json.workTime,
+              restTime: json.restTime,
+              timeLeft: json.timeLeft,
+            });
     })
-      .then((data) => {
-        const [workTime, restTime, isOn, timeLeft] = data.split("/");
-        this.setState({
-          isOnline: true,
-          workTime,
-          restTime,
-          isOn,
-          timeLeft,
-        });
-        console.log("online");
-      })
-      .catch((error) => {
-        console.error(error);
-        console.log("offline");
-        this.setState({ isOnline: false });
-      });
+  }
 
   componentDidMount() {
     setTimeout(() => this.setState({ isInitiallyLoading: false }), 1000);
     this.heartbeatAnimation();
-    setInterval(() => this.connect(), 5000);
+    setInterval(() => this.connect(), 3000);
   }
 
+  verboseName = miliseconds => {
+      const seconds = miliseconds / 1000;
+      if (seconds < 60)
+          return `${seconds} секунди`
+
+      return `${(seconds - seconds % 60) / 60} минути и ${seconds % 60} секунди`
+  }
+  timeLeftVerbose =  () => {
+      if (!this.state.timeLeft) {
+          return 'N/A'
+      }
+
+      if (this.state.isOn) {
+          return  'Изключване след ' + this.verboseName(this.state.timeLeft);
+      } else {
+          return  'Включване след ' + this.verboseName(this.state.timeLeft);
+      }
+  }
+
+  currentProgram = () => {
+      return (
+          <View style={{color: 'white'}}>
+              <Text style={{color: 'white'}}>Време работа: {this.state.workTime ? this.verboseName(this.state.workTime) : 'N/A'}</Text>
+              <Text style={{color: 'white'}}>Време почивка: {this.state.restTime ? this.verboseName(this.state.restTime) : 'N/A'}</Text>
+              <Text style={{color: 'white'}}>{this.timeLeftVerbose()}</Text>
+          </View>
+      )
+
+  }
   currentState = () => {
     if (this.state.isOn)
       return (
@@ -100,7 +131,7 @@ export default class App extends React.Component {
               height: bulbSize,
               marginTop: 20,
             }}
-            source={require("./assets/bulb-on.svg")}
+            source={require("./assets/workingLamp.jpg")}
           />
           <Text style={{ color: "#fff", textAlign: "center" }}>Включена</Text>
         </View>
@@ -137,10 +168,12 @@ export default class App extends React.Component {
         <Text style={styles.title}>Централен Балкан ЕООД</Text>
         <Text style={styles.title}>Бактерицидна UV лампа</Text>
         <View>
-          <Text style={styles.statusBoxLabel}>Статус на връзката</Text>
+            <Text style={styles.statusBoxLabel}>
+                Статус на връзката: {this.state.isOn ? "Има връзка с устройството" : "Няма връзка с устройството"}
+            </Text>
           <Animated.View
             style={{
-              backgroundColor: "#3aff3a",
+              backgroundColor: this.state.isOnline ? "#3aff3a" : 'red',
               opacity: this.state.statusBosAnimatedOpacity,
               width: 20,
               height: 20,
@@ -150,12 +183,16 @@ export default class App extends React.Component {
           ></Animated.View>
         </View>
         {this.currentState()}
+        {this.currentProgram()}
 
         <Text style={styles.subTitle}>Режими на работа</Text>
 
         <View style={styles.buttonBox}>
           <Button
-            title="5 минути работа на 2 часа"
+              title="
+              Търговска зала - интезивен 
+              (5 минути работа, 2 часа почивка)
+              "
             color={buttonColor}
             onPress={() => setConfig(5 * 60, 2 * 60 * 60)}
           />
@@ -163,7 +200,10 @@ export default class App extends React.Component {
 
         <View style={styles.buttonBox}>
           <Button
-            title="3 минути работа на 2 часа"
+              title="
+              Офис - интезивен 
+              (3 минути работа, 2 часа почивка)
+              "
             color={buttonColor}
             onPress={() => setConfig(3 * 60, 2 * 60 * 60)}
           />
