d62f3d98b8c51a07a39fc3a1b89acd3bd6867c6f
Ivo Donchev <i.donchev@hacksoft.io>
2020-12-07 19:46:23 +0200

    Add firmware emulator

---
+++ firmware/uv_lamp/mock.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins='*',
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/config/get/")
+async def read_item():
+    return {
+        "workTime": "5000",
+        "restTime": "5000",
+        "isCurrentlyWorkingInt": "0",
+        "timeLeft": "3576"
+    }
+
+
+@app.options("/config/get/")
+async def read_item():
+    return {
+        "workTime": "5000",
+        "restTime": "5000",
+        "isCurrentlyWorkingInt": "0",
+        "timeLeft": "3576"
+    }
